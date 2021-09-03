cbfba73c6b71bdf4df6cc0da50a6fdf92d60de46
petersonstadler2 <petersonstadler@gmail.com>
2021-09-03 16:20:02 -0300

    listarClientes
    Criado o método listar clientes em DAOCliente.

--- src/java/DAO/DAOCliente.java
+++ src/java/DAO/DAOCliente.java
@@ -2,6 +2,9 @@ package DAO;
 import MODEL.Cliente;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DAOCliente {
     
@@ -25,5 +28,23 @@ public class DAOCliente {
         }catch(Exception e){
             throw new RuntimeException("Erro ao inserir Cliente!", e);
         }
+    }
+    
+    public List<Cliente> listarClientes(){
+        List<Cliente> clientes = new ArrayList<>();
+        try{
+            String sql = "SELECT nome, email FROM tb_clientes";
+            stmt = conn.prepareStatement(sql);
+            ResultSet result = (ResultSet) stmt.executeQuery();
+            while(!result.next()){
+                Cliente cliente = new Cliente();
+                cliente.setNome(result.getString("nome"));
+                cliente.setEmail(result.getString("email"));
+                clientes.add(cliente);
+            }
+        }catch(Exception e){
+            throw new RuntimeException("Erro ao listar clientes!", e);
+        }
+        return clientes;
     }
 }
