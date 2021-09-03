478e7f5277d21876733ab78e4b092e305fcafb48
petersonstadler2 <petersonstadler@gmail.com>
2021-09-03 14:40:29 -0300

    Pacote de testes
    Adicionado pacote e classe de testes, e implementado um teste de conexão.

---
+++ src/java/TESTES/TestesPrincipal.java
@@ -0,0 +1,25 @@
+package TESTES;
+import DAO.Conexao;
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+
+public class TestesPrincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TestarConexao();
+    }
+    
+    private static void TestarConexao(){
+        try{
+            Connection conn = new Conexao().getConexao();
+            System.out.println("Conexão: passou!");
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            JOptionPane.showMessageDialog(null, e.getCause());
+            System.out.println("Conexão: não passou!");
+        }
+    }
+}
